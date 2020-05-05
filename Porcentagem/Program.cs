125fca4ff558bcfdb4bef90f0d7ba9d998e9ed93
DiegoVeiga <diegoroberto27@hotmail.com>
2020-05-05 08:18:40 -0300

    implementacao

---
+++ Porcentagem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porcentagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o nome de um produto");
+            string NomeProduto = Console.ReadLine();
+
+            Console.WriteLine("Entre com o preço do produto");
+            double preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("entre com a porcentagem de desconto");
+            double desconto = double.Parse(Console.ReadLine());
+
+
+            double valorfinal = preco * (desconto / 100);
+            Console.WriteLine(valorfinal);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
